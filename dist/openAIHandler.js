4aa6367cf42969a4a8d4409024eda7b27c1a1504
David Santana Cabrera <dvdsantana@gmail.com>
2024-02-22 15:35:29 +0100

    C

--- dist/openAIHandler.js
+++ dist/openAIHandler.js
@@ -29,9 +29,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.analyzeCode = void 0;
 const core = __importStar(require("@actions/core"));
 const openai_1 = __importDefault(require("openai"));
-//
 const OPENAI_API_KEY = core.getInput('OPENAI_API_KEY');
 const OPENAI_API_MODEL = core.getInput('OPENAI_API_MODEL');
+const OPENAI_API_PROMPT = core.getInput('OPENAI_API_PROMPT');
 const openai = new openai_1.default({
     apiKey: OPENAI_API_KEY
 });
@@ -58,13 +58,7 @@ async function analyzeCode(parsedDiff, prDetails) {
 }
 exports.analyzeCode = analyzeCode;
 function createPrompt(file, chunk, prDetails) {
-    return `Your task is to review pull requests. Instructions:
-  - Provide the response in following JSON format:  {'reviews': [{'lineNumber':  <line_number>, 'reviewComment': '<review comment>'}]}
-  - Do not give positive comments or compliments.
-  - Provide comments and suggestions ONLY if there is something to improve, otherwise 'reviews' should be an empty array.
-  - Write the comment in GitHub Markdown format.
-  - Use the given description only for the overall context and only comment the code.
-  - IMPORTANT: NEVER suggest adding comments to the code.
+    return `${OPENAI_API_PROMPT}
   
   Review the following code diff in the file '${file.to}' and take the pull request title and description into account when writing the response.
     
